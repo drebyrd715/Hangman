e5f041fa7a6863246b22f055b352f6a49f91ee2d
drebyrd715 <drebyrd715@gmail.com>
2023-11-09 15:08:32 -0500

    keyboard clicks works

--- script.js
+++ script.js
@@ -5,16 +5,21 @@ const keyboardDiv = document.querySelector(".keyboard");
 const getRandomWord = () => {
     /// getting random word from wordlist //////
   const { word, hint } = wordlist[Math.round(Math.random() * wordlist.length)];
-  console.log(word)
+  console.log(word);
   document.querySelector(".hint-text b").innerText = hint;
   wordDisplay.innerHTML = word.split("").map(() => `<li class="letter"><li>`).join("");
 };
 
-///// Making keyboard buttons for all letters////////
+const startGame = (button, clickedLetter) => {
+    console.log(clickedLetter)
+}
+
+///// Making keyboard buttons also event listeners////////
 for (let i = 97; i <= 122; i++) {
   const button = document.createElement("button");
   button.innerText = String.fromCharCode(i);
   keyboardDiv.appendChild(button);
+  button.addEventListener("click", e => startGame(e.target, String.fromCharCode(i)));
 }
 
 getRandomWord();
