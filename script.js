f6cffec9fde0ec907ccf66e3e9ebf0f8de09480e
drebyrd715 <drebyrd715@gmail.com>
2023-11-09 12:56:29 -0500

    made keyboard buttons

---
+++ script.js
@@ -0,0 +1,7 @@
+const keyboardDiv = document.querySelector(".keyboard");
+///// First make keyboard buttons for all letters////////
+for (let i = 97; i <= 122; i++){
+    const button = document.createElement("button");
+    button.innerText = String.fromCharCode(i);
+    keyboardDiv.appendChild(button);
+}
