f2b919aad4a6fa175109e44818e6d588f8e885e1
drebyrd715 <drebyrd715@gmail.com>
2023-11-09 13:55:13 -0500

    small updates

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-import {wordlist} from "./wordlist.js"
-console.log(wordlist)
+import { wordlist } from "./wordlist.js";
 const keyboardDiv = document.querySelector(".keyboard");
-///// Making keyboard buttons for all letters////////
-for (let i = 97; i <= 122; i++){
-    const button = document.createElement("button");
-    button.innerText = String.fromCharCode(i);
-    keyboardDiv.appendChild(button);
-}
-let word
-let hint
+
+let word;
+let hint;
 ///// Get Random word///////
 const getRandomWord = () => {
-     word = wordlist[Math.round(Math.random() * wordlist.length)];
-     hint = wordlist[Math.round(Math.random() * wordlist.length)];
-    console.log(word, hint)
+    /// getting random word from wordlist //////
+  word = wordlist[Math.round(Math.random() * wordlist.length)];
+  hint = wordlist[Math.round(Math.random() * wordlist.length)];
+};
+
+///// Making keyboard buttons for all letters////////
+for (let i = 97; i <= 122; i++) {
+  const button = document.createElement("button");
+  button.innerText = String.fromCharCode(i);
+  keyboardDiv.appendChild(button);
 }
 
 getRandomWord();
